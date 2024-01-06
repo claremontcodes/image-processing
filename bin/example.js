2f427f27e9329dd07f642cc5862954abc31fbc48
jareyes <josh@joshreyes.com>
2024-01-06 12:31:53 -0500

    Dithered

--- bin/example.js
+++ bin/example.js
@@ -4,7 +4,7 @@ const Image = require("../lib/image");
 
 async function main() {
   const image = await Image.open("./var/water-lily.png");
-  filter.tint(image, new Color(255, 0, 255));
+  filter.dither(image);
 
   image.save("./test.png");
 }
